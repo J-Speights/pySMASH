e2ed9e81b6c3aa5856fae58febaceb60dc26cfbc
J-Speights <jspeights@aegisfoundry.com>
2023-11-15 22:23:17 -0600

    Initial Commit
    All basic functionality is present. Simple UI.

---
+++ config.py
@@ -0,0 +1,14 @@
+HELP_TEXT = """How to use pySMASH:
+    \n 1. Set Desired Sleep times and iteration counts.
+    \n 2. Click Capture clicks to record a list of coords.
+    \n - RIGHT CLICK to stop capturing clicks for that list.
+    \n - Your list will populate under the Start Clicking button.
+    \n 3. Repeat for as many lists as needed.
+    \n 4. Click "Start Clicking" to begin the automated clicking process.
+    \n NOTE: Move mouse to a corner "quickly" to cancel process.
+    \n NOTE: ESC will also cancel process. 
+    """
+
+default_sleep_time = "1"
+default_sleep_between_lists_time = "3"
+default_iteration_count = "10"
---
+++ keyboard_listener.py
@@ -0,0 +1,30 @@
+from pynput import keyboard
+import tkinter as tk
+from typing import Union
+
+
+class StopFlag:
+    def __init__(self):
+        self.stop_requested = False
+
+
+def on_press(
+    key: Union[keyboard.Key, keyboard.KeyCode],
+    stop_flag: StopFlag,
+    status_label,
+    root: tk.Tk,
+) -> Union[None, bool]:
+    if key == keyboard.Key.esc:
+        stop_flag.stop_requested = True
+        status_label.config(text="User pressed Esc, Stopping...")
+        root.update_idletasks()
+        return False
+
+
+def start_keyboard_listener(
+    root: tk.Tk, stop_requested: StopFlag, status_label: tk.Label
+) -> None:
+    listener = keyboard.Listener(
+        on_press=lambda key: on_press(key, stop_requested, status_label, root)
+    )
+    listener.start()
