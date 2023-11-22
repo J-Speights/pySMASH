d958257db4d932ac622b991dcdcc19223ec336fb
J-Speights <jspeights@aegisfoundry.com>
2023-11-22 16:29:38 -0600

    feature additions
    Added features: Save and Load configuration, random vs sequential iteration. Button colors and spacing for UX.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from pynput import mouse
+from tkinter import filedialog
 
 import pyautogui
 import time
 import random
 import threading
+import json
 
 from keyboard_listener import StopFlag, start_keyboard_listener
 from config import (
@@ -68,9 +70,17 @@ def click_coordinates(coordinates, sleep_time, stop_flag: StopFlag):
 
 
 def handle_multi_list(
-    list_of_list_of_coords, sleep_time, sleep_between_lists_time, stop_flag: StopFlag
+    list_of_list_of_coords,
+    sleep_time,
+    sleep_between_lists_time,
+    stop_flag: StopFlag,
+    use_random_order: bool,
+    current_index: int = 0,
 ):
-    selected = random.choice(list_of_list_of_coords)
+    if use_random_order:
+        selected = selected = random.choice(list_of_list_of_coords)
+    else:
+        selected = list_of_list_of_coords[current_index]
     click_result = click_coordinates(selected, sleep_time, stop_flag)
     time.sleep(sleep_between_lists_time)
     return click_result
@@ -90,6 +100,7 @@ def start_clicking(stop_flag: StopFlag):
         sleep_time = float(sleep_time_entry.get())
         sleep_between_lists_time = float(sleep_between_lists_entry.get())
         iteration_count = int(iteration_count_entry.get())
+        use_random_order = use_random_order_var.get() == 1
 
         for i in range(iteration_count):
             status_label.config(
@@ -97,16 +108,73 @@ def start_clicking(stop_flag: StopFlag):
             )
             root.update_idletasks()
 
-            if not handle_multi_list(
-                list_of_list_of_coords, sleep_time, sleep_between_lists_time, stop_flag
-            ):
-                return
+            if use_random_order:
+                for _ in list_of_list_of_coords:
+                    if not handle_multi_list(
+                        list_of_list_of_coords,
+                        sleep_time,
+                        sleep_between_lists_time,
+                        stop_flag,
+                        use_random_order,
+                    ):
+                        return
+            else:
+                for j in range(len(list_of_list_of_coords)):
+                    if not handle_multi_list(
+                        list_of_list_of_coords,
+                        sleep_time,
+                        sleep_between_lists_time,
+                        stop_flag,
+                        use_random_order,
+                        j,
+                    ):
+                        return
 
         status_label.config(text="Finished clicking.")
     except ValueError:
         print("Invalid entries.")
     except SyntaxError:
         print("Invalid coords.")
+
+
+def save_configuration() -> None:
+    filename = filedialog.asksaveasfilename(
+        defaultextension=".json",
+        filetypes=[("JSON files", "*.json"), ("All files", "*.*")],
+    )
+    if filename:
+        config = {
+            "list_of_list_of_coords": list_of_list_of_coords,
+            "sleep_time": sleep_time_entry.get(),
+            "sleep_between_lists_time": sleep_between_lists_entry.get(),
+            "iteration_count": iteration_count_entry.get(),
+        }
+        with open(filename, "w") as config_file:
+            json.dump(config, config_file, indent=4)
+
+
+def load_configuration(filename="config.json"):
+    filename = filedialog.askopenfilename(
+        filetypes=[("JSON files", "*.json"), ("All files", "*.*")]
+    )
+    if filename:
+        try:
+            with open(filename, "r") as config_file:
+                config = json.load(config_file)
+                global list_of_list_of_coords
+
+                list_of_list_of_coords = config["list_of_list_of_coords"]
+                sleep_time_entry.delete(0, tk.END)
+                sleep_time_entry.insert(0, config["sleep_time"])
+                sleep_between_lists_entry.delete(0, tk.END)
+                sleep_between_lists_entry.insert(0, config["sleep_between_lists_time"])
+                iteration_count_entry.delete(0, tk.END)
+                iteration_count_entry.insert(0, config["iteration_count"])
+                update_coord_display()
+        except (FileNotFoundError, json.JSONDecodeError):
+            status_label.config(
+                text="Config File not found or invalid. Loading default values."
+            )
 
 
 def show_help():
@@ -127,8 +195,10 @@ if __name__ == "__main__":
 
     stop_flag = StopFlag()
 
-    help_button = tk.Button(root, text="Help", command=show_help)
-    help_button.grid(row=0, columnspan=2)
+    help_button = tk.Button(
+        root, text="? Help ?", command=show_help, width=20, height=1, bg="light yellow"
+    )
+    help_button.grid(row=0, columnspan=2, pady=(5, 5))
 
     tk.Label(root, text="Sleep Time between clicks (sec):").grid(row=1)
     sleep_time_entry = tk.Entry(root)
@@ -145,24 +215,67 @@ if __name__ == "__main__":
     iteration_count_entry.grid(row=3, column=1)
     iteration_count_entry.insert(0, default_iteration_count)
 
-    capture_button = tk.Button(root, text="Capture Clicks", command=start_capturing)
-    capture_button.grid(row=4, columnspan=2)
-
-    start_button = tk.Button(
-        root, text="Start Clicking", command=lambda: start_clicking(stop_flag)
+    use_random_order_var = tk.IntVar()
+    use_random_order_checkbox = tk.Checkbutton(
+        root, text="Use Random Order", variable=use_random_order_var
     )
-    start_button.grid(row=5, columnspan=2)
+    use_random_order_checkbox.grid(row=4, columnspan=2)
+
+    capture_button = tk.Button(
+        root,
+        text="Capture Clicks",
+        command=start_capturing,
+        width=20,
+        height=2,
+        bg="light blue",
+    )
+    capture_button.grid(row=5, columnspan=2, pady=(10, 5))
 
     coord_display = tk.Listbox(root, height=10, width=50)
     coord_display.grid(row=6, columnspan=2)
 
-    delete_button = tk.Button(
-        root, text="Delete Selected Set", command=delete_selected_set
+    start_button = tk.Button(
+        root,
+        text="Start Clicking",
+        command=lambda: start_clicking(stop_flag),
+        width=20,
+        height=2,
+        bg="light green",
     )
-    delete_button.grid(row=7, columnspan=2)
+    start_button.grid(row=7, columnspan=2, pady=(10, 0))
+
+    delete_button = tk.Button(
+        root,
+        text="Delete Selected Set",
+        command=delete_selected_set,
+        width=20,
+        height=1,
+        bg="light pink",
+    )
+    delete_button.grid(row=10, columnspan=2, pady=(10, 5))
+
+    save_config = tk.Button(
+        root,
+        text="Save Config",
+        width=20,
+        height=1,
+        bg="light yellow",
+        command=save_configuration,
+    )
+    save_config.grid(row=11, column=0, pady=(10, 5), padx=(5, 5))
+
+    load_config = tk.Button(
+        root,
+        text="Load Config",
+        width=20,
+        height=1,
+        bg="light yellow",
+        command=load_configuration,
+    )
+    load_config.grid(row=11, column=1, pady=(10, 5), padx=(5, 5))
 
     status_label = tk.Label(root, text="")
-    status_label.grid(row=8, columnspan=2)
+    status_label.grid(row=12, columnspan=2)
 
     start_keyboard_listener(root, stop_flag, status_label)
 
